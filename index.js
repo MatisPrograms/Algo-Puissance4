276158f0a7fada93d2504d2ee9578d5ce92cee67
MatisPrograms <matis_herrmann@hotmail.fr>
2023-03-31 13:33:12 +0200

    Added Montecarlo

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const app = require('express')();
 
+const maxSeconds = 2;
+
+const monteCarlo = require('./monteCarlo')(maxSeconds);
+
 const lines = 6;
 const colums = 7;
 const board = [
@@ -21,6 +25,7 @@ app.get('/', (req, res) => {
 
 app.get('/move', (req, res) => {
     parseUrlBoard(board, req.query.b);
+    printBoard(board);
 
     res.send({
         column: 4
@@ -40,8 +45,28 @@ function parseUrlBoard(board, string) {
     // Check if string only contains M, H or 0
     if (!string.match(/^[MH0]+$/)) return;
 
-    // Split string into array of 6 arrays of 7 characters and store it in board
-    for (let i = 0; i < lines; i++) {
-        board[i] = string.substr(i * colums, colums).split('');
+
+    // m00000h00000mm0000hmh000h00000h00000000000 to
+    // 0 0 0 0 0 0 0
+    // 0 0 0 0 0 0 0
+    // 0 0 0 0 0 0 0
+    // 0 0 0 H 0 0 0
+    // 0 0 M M 0 0 0
+    // M H M H H H 0
+
+    // convert string to board
+    let indexC = -1;
+    for (const c of string) {
+        indexC++;
+        if (c === '0') continue;
+        console.log(Math.floor(indexC / colums), indexC % colums);
+        board[Math.floor(indexC / colums)][indexC % colums] = c;
+    }
+}
+
+function printBoard(board) {
+    // 0 0 is bottom left and 5 6 is top right
+    for (let i = lines - 1; i >= 0; i--) {
+        console.log(board[i].join(' '));
     }
 }
