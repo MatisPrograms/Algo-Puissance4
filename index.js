a741b5498a8d15aa70554bf61d590d9add0cd39a
MatisPrograms <matis_herrmann@hotmail.fr>
2023-03-31 11:46:32 +0200

    Added Conversion of url board parameter to a board object

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
-const express = require('express');
+const app = require('express')();
 
-const app = express();
+const lines = 6;
+const colums = 7;
+const board = [
+    ['0', '0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0', '0']
+];
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Server is running');
 });
 
-grille = []
-const lines = 6;
-const colums = 7;
+app.get('/move', (req, res) => {
+    parseUrlBoard(board, req.query.b);
+
+    res.send({
+        column: 4
+    });
+});
 
 
-const convertEntry = (string) => {
-    let i = 0;
-    let tmp = [];
-    for (i in colums){
-        tmp = string.splice(0,6)
-        tmp = tmp.split(",")
-        grille[i] = tmp;
+function parseUrlBoard(board, string) {
+    // Check if board is valid
+    if (!board) return;
+
+    // Check if string length is valid
+    if (string.length !== 42) return;
+
+    string = string.toUpperCase();
+
+    // Check if string only contains M, H or 0
+    if (!string.match(/^[MH0]+$/)) return;
+
+    // Split string into array of 6 arrays of 7 characters and store it in board
+    for (let i = 0; i < lines; i++) {
+        board[i] = string.substr(i * colums, colums).split('');
     }
-    return grille;
 }
-
-convertEntry("m00000h00000mm0000hmh000h00000h00000000000");
