95535ddd3bdc6275bb1a4e98ac35a2084664230c
Matis <matis_herrmann@hotmail.fr>
2023-04-07 11:25:18 +0200

    Board correctly stored and added a print output function

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const app = require('express')();
 
 const maxSeconds = 2;
 
-const monteCarlo = require('./monteCarlo')(maxSeconds);
+const monteCarlo = new (require('./monteCarlo'))(maxSeconds);
 
 const lines = 6;
 const colums = 7;
+
 const board = [
-    ['0', '0', '0', '0', '0', '0', '0'],
-    ['0', '0', '0', '0', '0', '0', '0'],
-    ['0', '0', '0', '0', '0', '0', '0'],
-    ['0', '0', '0', '0', '0', '0', '0'],
-    ['0', '0', '0', '0', '0', '0', '0'],
-    ['0', '0', '0', '0', '0', '0', '0']
+    ['0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0']
 ];
 
 app.listen(3000, () => {
@@ -45,28 +47,17 @@ function parseUrlBoard(board, string) {
     // Check if string only contains M, H or 0
     if (!string.match(/^[MH0]+$/)) return;
 
-
-    // m00000h00000mm0000hmh000h00000h00000000000 to
-    // 0 0 0 0 0 0 0
-    // 0 0 0 0 0 0 0
-    // 0 0 0 0 0 0 0
-    // 0 0 0 H 0 0 0
-    // 0 0 M M 0 0 0
-    // M H M H H H 0
-
     // convert string to board
     let indexC = -1;
     for (const c of string) {
         indexC++;
         if (c === '0') continue;
-        console.log(Math.floor(indexC / colums), indexC % colums);
-        board[Math.floor(indexC / colums)][indexC % colums] = c;
+        board[Math.floor(indexC / lines)][indexC % lines] = c;
     }
 }
 
 function printBoard(board) {
-    // 0 0 is bottom left and 5 6 is top right
-    for (let i = lines - 1; i >= 0; i--) {
-        console.log(board[i].join(' '));
-    }
+    console.table(board[0].map((col, i) => board.map(row => row[i])).reverse());
 }
+
+fetch('http://localhost:3000/move?b=m00000h00000mm0000hmh000h00000h00000000000').then(res => res.json()).then(console.log);
--- monteCarlo.js
+++ monteCarlo.js
@@ -1,8 +1,21 @@
-module.exports = (maxSeconds) => {
-    let startTime;
-
-
-    function calculateNextMove(board) {
-
+// Define the MonteCarlo class
+class MonteCarlo {
+    constructor(maxSeconds) {
+        // Initialize any necessary variables/constants specific to the Monte Carlo algorithm
+        // ...
     }
-};
+
+    // Define any necessary helper functions specific to the Monte Carlo algorithm
+    // ...
+
+    calculateNextMove(board) {
+        // Implement the Monte Carlo algorithm to calculate the next move on the board
+        // ...
+
+        // Return the calculated next move
+        return 3;
+    }
+}
+
+// Export for use in other modules/files
+module.exports = MonteCarlo;
